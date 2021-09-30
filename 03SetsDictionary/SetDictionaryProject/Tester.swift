28a94d138550e818c763525cf1a2a9f2602cf68c
Olivia <olivia@MacBook-Pro.local>
2021-09-30 11:54:39 -0400

    setDictionary Project

--- 03SetsDictionary/SetDictionaryProject/Tester.swift
+++ 03SetsDictionary/SetDictionaryProject/Tester.swift
@@ -10,35 +10,84 @@ import Foundation
 struct Tester {
     /// Remove an array with the duplicated values removed
     func removeDups(nums: [Int]) -> [Int] {
-        
-        return []
+        let uniqueNum = Set(nums).sorted()
+        return Array(uniqueNum)
     }
     
     /// Find the nums that exist in both arrays
     func findCommonNums(from firstNums: [Int], and secondNums: [Int]) -> [Int] {
-        
-        return []
+        let firstNums = firstNums
+        let secondNums = secondNums
+        var lookup = [Int:Bool]()
+        firstNums.forEach {
+            lookup[$0] = true
+        }
+        var commonNums = [Int]()
+        secondNums.forEach {
+            num in
+            if lookup[num] ?? false {
+                commonNums.append(num)
+            }
+        }
+        return commonNums
     }
     
     /// first: "hello", second: "bye" -> ["e"]
     func findMatchingLetters(firstWord: String, secondWord: String) -> [Character] {
-        return []
+        let firstWord = Array(firstWord)
+        let secondWord = Array(secondWord)
+        var lookup = [Character:Bool]()
+        firstWord.forEach {
+            lookup[$0] = true
+        }
+        var commonLetters = [Character]()
+        secondWord.forEach {
+            letter in
+            if lookup[letter] ?? false {
+                commonLetters.append(letter)
+            }
+        }
+        return commonLetters
     }
     
     
     /// Create a dictionary of the counts of the letters, ignoring spaces
     func createCounter(string: String) -> [Character: Int] {
-        return [:]
+        let freedSpaceString = string.filter {!$0.isWhitespace}
+        let stringArr = Array(freedSpaceString)
+        var counter = [Character:Int]()
+        for letter in freedSpaceString {
+            counter[letter] = (counter[letter] ?? 0) + 1
+        }
+           return counter
     }
     
     /// Find most frequent letter in string: "hello there" -> "e"
     func mostFrequentLetter(string: String) -> Character {
-        return "."
+        let freedSpaceString = string.filter {!$0.isWhitespace}
+        let stringArr = Array(freedSpaceString)
+        var counter = [Character:Int]()
+        var character: Character = " "
+        for letter in freedSpaceString {
+        counter[letter] = (counter[letter] ?? 0) + 1
+        }
+        if let (letter, count) = counter.max(by: {$0.1 < $1.1}){
+            character = letter
+        }
+        return character
     }
     
     /// O(n) time: avoid nested for loops
     func findPairs(nums: [Int]) -> Int {        
-        return 1
+        var pair = 0
+        for i in 0..<nums.count{
+            for j in 1..<nums.count {
+                if(nums[i] + nums[j] == 0){
+                    pair += 1
+                }
+            }
+        }
+        return pair
     }
     
     /**
@@ -48,7 +97,16 @@ struct Tester {
      [1, 4, 8, 1] -> [1, 2, 3, 1]
      */
     func reduceDistanceKeepPriority(array: [Int]) -> [Int] {
-        return [1]
+        let mySet = Set(array)
+        let sortedNums = mySet.sorted()
+        var dictionary = [Int: Int]()
+        var priorityArr = [Int]()
+        for i in 0..<sortedNums.count{
+            dictionary[sortedNums[i]] = i+1
+        }
+        for num in array {
+            priorityArr.append(dictionary[num] ?? 0)
+        }
+    return priorityArr
     }
-    
 }
