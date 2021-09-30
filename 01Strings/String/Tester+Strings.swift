809a26d19a551515dd8f56939ef81a58b38d472d
Daniel Brannon <danielbrannon@daniels-air.lan>
2021-09-29 21:55:51 -0400

    finished string project

--- 01Strings/String/Tester+Strings.swift
+++ 01Strings/String/Tester+Strings.swift
@@ -11,28 +11,38 @@ extension Tester {
 
     /// Using print command
     func printWord(word: String) {
-        
+        print(word)
     }
     
     /// input "hello" should print "HELLO"
     func printUpperCase(word: String) {
-        
+        print(word.uppercased())
     }
     
     /// Using for loop, print each char in word
     func printLetters(word: String) {
-        
+        for letter in word {
+            print(letter)
+        }
     }
     
     /// given a word, flip its characters, and print the result
     /// e.g. football -> llabtoof
     func flipWord(word: String) {
-        
+        let reversedWord = String(word.reversed())
+        print(reversedWord)
     }
     
     /// Using for-in-enumerated(), print odd-position chars in word
     func printOddCharsEnumerated(word: String) {
-        
+//        var oddWord: [String] = []
+        for (index, value) in word.enumerated() {
+            if index % 2 == 0 {
+//                oddWord.append(String(value))
+                print(value)
+            }
+        }
+//        print(oddWord)
     }
     
     /**
@@ -44,11 +54,23 @@ extension Tester {
      one -> oneyay
      */
     func printPigLatin(word: String) {
-        
+        if word[word.startIndex] == "a" || word[word.startIndex] == "e" || word[word.startIndex] == "i" || word[word.startIndex] == "o" || word[word.startIndex] == "u" || word[word.startIndex] == "A" || word[word.startIndex] == "E" || word[word.startIndex] == "I" || word[word.startIndex] == "O" || word[word.startIndex] == "U" {
+            print(word + "yay")
+        } else {
+            let secondIndex = word.index(word.startIndex, offsetBy: 1)
+            let lastIndex = word.index(word.startIndex, offsetBy: word.count - 1)
+            var newWord = word[secondIndex...lastIndex]
+            newWord.append(word[word.startIndex])
+            print(newWord + "ay")
+        }
     }
-    
+
     /// Using loop and string.index, return true if input is a palindrome
     func isPalindrome(word: String) -> Bool {
+        let reversedWord = String(word.reversed())
+        if reversedWord == word {
+            return true
+        }
         return false
     }
 }
